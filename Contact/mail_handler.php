83c9add2a2d6bb11d0679ea92c92a58e574e2cbb
kamaldeep56 <kamaldeepsinghdhanjal@gmail.com>
2020-10-08 04:20:57 +0530

    Update mail_handler.php

--- Contact/mail_handler.php
+++ Contact/mail_handler.php
@@ -5,7 +5,7 @@
 		$phone=$_POST['phone'];
 		$msg=$_POST['msg'];
 
-		$to='xyz.123@mail.com'; // Receiver Email ID, Replace with your email ID
+		$to='kamaldeepsinghdhanjal@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
